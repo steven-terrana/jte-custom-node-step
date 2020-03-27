72da0ff92815d37b57f6d272a2d14d0af548f41d
steven-terrana <steventerrana@gmail.com>
2020-03-27 12:14:58 -0400

    test

--- utility/node.groovy
+++ utility/node.groovy
@@ -1,5 +1,5 @@
 void call(Closure body){
-    def bodyLibConfig = body.getOwner().getConfig()
+    def bodyLibConfig = body.getDelegate().getConfig()
 
     def nodeLabel = bodyLibConfig.label ?: config.label ?: "" 
     println "would use node label: ${nodeLabel}"
